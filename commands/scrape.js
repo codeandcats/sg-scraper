860f209c5484af4c3308c8193d2f93a349a534c3
Ben <codeandcats@gmail.com>
2016-03-21 19:47:15 +1100

    Resolves #2 by adding ability to grab album url from clipboard if url is not specified when running scrape

--- commands/scrape.js
+++ commands/scrape.js
@@ -3,12 +3,32 @@ var scraper = require('../scraper');
 var utils = {
 	console: require('../utils/console')
 };
+var clipBoard = require('copy-paste');
 
 cli
-    .command('scrape <album-url>')
+    .command('scrape [album-url]')
     .action(url => {
-		scraper.scrapeAlbum(url)
-			.then(() => utils.console.exit())
-			.catch(utils.console.exit);
+		if (!url) {
+			clipBoard.paste((err, text) => {
+				if (!err && isValidUrl(text)) {
+					scrape(text);
+				}
+				else {
+					utils.console.exit('Url required as argument or in clipboard.');
+				}
+			});
+		}
+		else {
+			scrape(url);
+		}
     });
 
+function scrape(url) {
+	scraper.scrapeAlbum(url)
+		.then(() => utils.console.exit())
+		.catch(utils.console.exit);
+}
+
+function isValidUrl(url) {
+	return (url || '').match(/(https:\/\/){0,1}www.suicidegirls.com\/.*/i);
+}
